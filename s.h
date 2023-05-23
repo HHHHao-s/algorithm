c33d27c3133b7dc976aa94a3531463cac993c521
曾荣豪 <2755942193@qq.com>
2023-05-23 19:34:53 +0800

    2023.5.23 休息，只写了一题

--- s.h
+++ s.h
@@ -62,3 +62,16 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
     ListNode(int x, ListNode *_next) : val(x), next(_next){}
 };
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
