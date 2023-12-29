5318e2d2ec6ab6ed1586b8c1497e164ab4478bbb
曾荣豪 <2755942193@qq.com>
2023-12-29 10:46:32 +0800

    2023.12.29

---
+++ 2223.构造字符串的总得分和.cpp
@@ -0,0 +1,25 @@
+#include "s.h"
+/*
+ * @lc app=leetcode.cn id=2223 lang=cpp
+ *
+ * [2223] 构造字符串的总得分和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    long long sumScores(string s) {
+        int n = s.size();
+        long long ans;
+
+        int r = 0;
+        int l = 0;
+        while(r<n){
+            if(s[r]==s[l]){
+                
+            }
+        }
+    }
+};
+// @lc code=end
+
