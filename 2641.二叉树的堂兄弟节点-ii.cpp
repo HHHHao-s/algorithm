b2e11819e953aa00a929792d9776f15ec5b8cf53
曾荣豪 <2755942193@qq.com>
2024-02-07 08:48:35 +0800

    2024.2.7

---
+++ 2641.二叉树的堂兄弟节点-ii.cpp
@@ -0,0 +1,55 @@
+#include "s.h"
+/*
+ * @lc app=leetcode.cn id=2641 lang=cpp
+ *
+ * [2641] 二叉树的堂兄弟节点 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        vector<TreeNode*> q = {root};
+        root->val = 0;
+        while (!q.empty()) {
+            vector<TreeNode*> q2;
+            int sum = 0;
+            for (auto fa : q) {
+                if (fa->left) {
+                    q2.push_back(fa->left);
+                    sum += fa->left->val;
+                }
+                if (fa->right) {
+                    q2.push_back(fa->right);
+                    sum += fa->right->val;
+                }
+            }
+            for (auto fa : q) {
+                int child_sum = (fa->left ? fa->left->val : 0) + 
+                                (fa->right ? fa->right->val : 0);
+                if (fa->left) {
+                    fa->left->val = sum - child_sum;
+                }
+                if (fa->right) {
+                    fa->right->val = sum - child_sum;
+                }
+            }
+            q = move(q2);
+        }
+        return root;
+    }
+};
+
+// @lc code=end
+
