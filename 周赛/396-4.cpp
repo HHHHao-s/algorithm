3f4498188b0bdeba4d1837d5d8d497f5663f09be
曾荣豪 <2755942193@qq.com>
2024-05-05 12:59:12 +0800

    modify

--- 周赛/396-4.cpp
+++ 周赛/396-4.cpp
@@ -3,61 +3,45 @@
 class Solution {
 public:
     int minCostToEqualizeArray(vector<int>& nums, int cost1, int cost2) {
+        long long n = nums.size();
         long long  mx = *max_element(nums.begin(), nums.end());
         long long ans = 0;
         const long long MOD = 1e9+7;
-        sort(nums.begin() , nums.end(), greater<>());
+        sort(nums.begin() , nums.end());
         if(cost2>=2*cost1){
             for(int num: nums){
                 ans = (ans+(mx-num)*cost1 )%MOD;
             }
             if(ans <0){
-                ans+=MOD;
+                ans+=MOD;       
+            }
+            
+            return ans;
+        } 
+        long long sum = 0;
+        for(int i=1;i<n;i++){
+            sum += mx-nums[i];
+        }
+        ans = LONG_LONG_MAX;
+        for(int i=0;i<1e6;i++){
+            long long l = i+mx - nums[0];
+            long long r = sum+(n-1)*i;
+            long long tmp=0;
+            if(l<=r){
+                tmp = (l+r)/2*cost2;
+                if((l+r)%2==1){
+                    tmp += cost1;
+                }
+                
+            }else{
+                tmp = r*cost2;
+                tmp = (tmp+ (l-r)*cost1 );
                 
             }
-            return ans;
+            ans = min(ans, tmp);
         }
         
-
-        long long used = 0;
-        long long last = 0;
-        int cur = 0;
-        for(int i=0;i<nums.size()-1;i++){
-            int num = nums[i];
-            num+=last;
-            last = mx - num;
-            used += mx-num;
-            ans = (ans+ (mx-num)*cost2 )%MOD;
-        }
-        int last_num = nums.back();
-        last_num+=last;
-        if(mx-last_num <= used){
-            if((mx-last_num) %2==0){
-                ans = (ans+ ((mx-last_num)/2 * cost2)%MOD)%MOD;
-            }else{
-                
-                ans = (ans+ ((mx-last_num)/2 * cost2)%MOD)%MOD;
-                if((nums.size()-1) %2 ==0){
-                    ans =( ans + min((long long)cost1,((long long)((nums.size()+1)/2*cost2 )))%MOD);
-                }else{
-                    ans = (ans+cost1)%MOD;
-                }
-            }
-        }else{
-            last_num+=used*2;
-            long long mn =( mx-last_num)*cost1;
-            for(int i=0;i<mx-last_num;i++){
-                if((mx-last_num-i + nums.size() )%2==0 && (mx-last_num-i<nums.size()-1)){
-                    mn = min(mn,(long long) (cost1*i + (mx-last_num-i + nums.size())/2*cost2)%MOD);
-                }
-            }
-            ans =(ans + mn)%MOD;
-        }
-        if(ans <0){
-            ans+=MOD;
-        }
-
-        return ans;
+        return ans%MOD;
     }
 };
 
